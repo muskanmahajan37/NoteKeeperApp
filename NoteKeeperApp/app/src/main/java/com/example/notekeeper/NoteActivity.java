fcce60d0bde6c769b52fc0c75829aa1351af6bb4
Batchu Thrishna <batchuthrishna@gmail.com>
2020-04-15 21:35:11 +0530

    Removed null exception in lines 44 & 49 NoteActivity.java

--- NoteKeeperApp/app/src/main/java/com/example/notekeeper/NoteActivity.java
+++ NoteKeeperApp/app/src/main/java/com/example/notekeeper/NoteActivity.java
@@ -21,6 +21,7 @@ import java.util.List;
 public class NoteActivity extends AppCompatActivity {
     public static final String NOTE_INFO = "com.example.notekeeper.NOTE_INFO";
     private NoteInfo mNote;
+    private boolean mIsNewNote;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,8 +41,9 @@ public class NoteActivity extends AppCompatActivity {
 
         EditText textNoteTitle = findViewById(R.id.text_note_title);
         EditText textNoteText = findViewById(R.id.text_note_text);
-        
-        displayNote(spinnerCourses, textNoteTitle, textNoteText);
+        if(!mIsNewNote) {
+            displayNote(spinnerCourses, textNoteTitle, textNoteText);
+        }
     }
 
     private void displayNote(Spinner spinnerCourses, EditText textNoteTitle, EditText textNoteText) {
@@ -55,6 +57,7 @@ public class NoteActivity extends AppCompatActivity {
     private void readDisplayStateValues() {
         Intent intent = getIntent();
         mNote = intent.getParcelableExtra(NOTE_INFO);
+        mIsNewNote = mNote == null;
     }
 
     @Override
